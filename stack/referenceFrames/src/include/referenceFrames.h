4774eed6a9d75686f3526b6844510471468bbbf0
joselusl <jl.sanchez@upm.es>
2014-09-26 14:04:31 +0200

    catkinizing the stack

--- stack/referenceFrames/src/include/referenceFrames.h
+++ stack/referenceFrames/src/include/referenceFrames.h
@@ -10,10 +10,6 @@
 
 
 
-////// ROS  ///////
-#include "ros/ros.h"
-
-
 
 
 //Cpp
@@ -24,36 +20,26 @@
 
 
 
-//Messages
-#include "droneMsgs/dronePose.h"
-
 #include <opencv2/core/core.hpp>        // Basic OpenCV structures (cv::Mat, Scalar)
 #include <opencv2/opencv.hpp>
 
 namespace referenceFrames
 {
 
-//Paloma 2 Sist ref global
-int refFrameChangeXYZ2ZYX(droneMsgs::dronePose pose_xYyPzR, droneMsgs::dronePose* pose_zYyPxR); //mobile robots 2 common
-int refFrameChangeZYX2XYZ(droneMsgs::dronePose pose_zYyPxR, droneMsgs::dronePose* pose_xYyPzR); //common 2 mobile robots
-
-// xYyPzR 2 wYvPuR 
-int xYyPzR2wYvPuR(droneMsgs::dronePose pose_xYyPzR, droneMsgs::dronePose* pose_wYvPuR); // GMR xYyPzR 2 GMR wYvPuR
-// wYvPuR 2 xYyPzR
-int wYvPuR2xYyPzR(droneMsgs::dronePose pose_wYvPuR, droneMsgs::dronePose* pose_xYyPzR); // GMR wYvPuR 2 GMR xYyPzR (mine_p)
-
-
 
 //Otros
 
 // Funciones auxiliares
-int createHomogMatrix_wYvPuR(cv::Mat *pHomogMat, double x, double y, double z, double yaw, double pitch, double roll);
+int createRotMatrix_wYvPuR(cv::Mat * const pRotMat, double yaw, double pitch, double roll);
+int createHomogMatrix_wYvPuR(cv::Mat * const pHomogMat, double x, double y, double z, double yaw, double pitch, double roll);
 inline int createHomogMatrix_xYyPzR(cv::Mat *pHomogMat, double x, double y, double z, double yaw, double pitch, double roll) { return createHomogMatrix_wYvPuR( pHomogMat, x, y, z, roll, pitch, yaw); }
 int getxyzYPRfromHomogMatrix_wYvPuR(const cv::Mat &HomogMat, double *px, double *py, double *pz, double *pyaw, double *ppitch, double *proll);
 int getxyzYPRfromHomogMatrix_xYyPzR(const cv::Mat &HomogMat, double *px, double *py, double *pz, double *pyaw, double *ppitch, double *proll);
 
 // from arUcoDefinition.cpp
 int createMatHomogFromVecs(cv::Mat* MatHomog, const cv::Mat &TransVec, const cv::Mat &RotVec);
+
+int invertHomogMatrix(const cv::Mat &pHomogMat_orig, cv::Mat &pHomogMat_target);
 }
         
         
--- stack/referenceFrames/src/sources/referenceFrames.cpp
+++ stack/referenceFrames/src/sources/referenceFrames.cpp
@@ -7,135 +7,37 @@
 
 
 #include "referenceFrames.h"
-#include "htrans.h"
+//#include "htrans.h"
 #include <cmath>
-#include <ctgmath>
 
 namespace referenceFrames
 {
-int refFrameChangeXYZ2ZYX(droneMsgs::dronePose pose_xYyPzR, droneMsgs::dronePose* pose_zYyPxR) //mobile robots 2 common
-{  
-    float x_input = pose_xYyPzR.x;
-    float y_input = pose_xYyPzR.y;
-    float z_input = pose_xYyPzR.z;
-    float roll_input = pose_xYyPzR.roll;
-    float pitch_input = pose_xYyPzR.pitch;
-    float yaw_input = pose_xYyPzR.yaw;
-    
-    HomogTrans h;
-    h.DirectTrans(x_input,y_input,z_input,roll_input,pitch_input,yaw_input);
-    
-    float x_output = x_input;
-    float y_output = y_input;
-    float z_output = z_input;
-    float roll_output = atan2(-h.mat[1][2],h.mat[2][2]);
-    float pitch_output = asin(h.mat[0][2]);
-    float yaw_output = acos(h.mat[0][0]/cos(pitch_output));
-    
-    pose_zYyPxR->x = x_output;
-    pose_zYyPxR->y = y_output;
-    pose_zYyPxR->z = z_output;
-    pose_zYyPxR->roll = roll_output;
-    pose_zYyPxR->pitch = pitch_output;
-    pose_zYyPxR->yaw = yaw_output;
-    
-    
-    return 1;
+
+int createRotMatrix_wYvPuR(cv::Mat * const pRotMat, double yaw, double pitch, double roll) {
+    int error_ocurred = 0;
+
+    if ( (pRotMat == NULL) || (pRotMat->empty()) || (pRotMat->size() != cv::Size(3,3)) ) {
+        error_ocurred = 1;
+        return error_ocurred;
+    }
+
+    pRotMat->at<float>(0,0) = cos(pitch)*cos(yaw);
+    pRotMat->at<float>(1,0) = cos(pitch)*sin(yaw);
+    pRotMat->at<float>(2,0) = -sin(pitch);
+
+    pRotMat->at<float>(0,1) = cos(yaw)*sin(pitch)*sin(roll) - cos(roll)*sin(yaw);
+    pRotMat->at<float>(1,1) = cos(roll)*cos(yaw) + sin(pitch)*sin(roll)*sin(yaw);
+    pRotMat->at<float>(2,1) = cos(pitch)*sin(roll);
+
+    pRotMat->at<float>(0,2) = sin(roll)*sin(yaw) + cos(roll)*cos(yaw)*sin(pitch);
+    pRotMat->at<float>(1,2) = cos(roll)*sin(pitch)*sin(yaw) - cos(yaw)*sin(roll);
+    pRotMat->at<float>(2,2) = cos(pitch)*cos(roll);
+
+    return error_ocurred;
 }
 
-int refFrameChangeZYX2XYZ(droneMsgs::dronePose pose_zYyPxR, droneMsgs::dronePose* pose_xYyPzR) //common 2 mobile robots
-{
-    
-    float x_input = pose_zYyPxR.x;
-    float y_input = pose_zYyPxR.y;
-    float z_input = pose_zYyPxR.z;
-    float roll_input = pose_zYyPxR.roll;
-    float pitch_input = pose_zYyPxR.pitch;
-    float yaw_input = pose_zYyPxR.yaw;
-    
-    HomogTrans h;
-    h.DirectTrans(x_input,y_input,z_input,roll_input,pitch_input,yaw_input);
-    
-    float x_output = x_input;
-    float y_output = y_input;
-    float z_output = z_input;
-    float roll_output = atan2(h.mat[1][0],h.mat[0][0]);
-    float pitch_output = atan2(-h.mat[2][0],cos(roll_output)*h.mat[0][0]+sin(roll_output)*h.mat[1][0]);
-    float yaw_output = atan2(sin(roll_output)*h.mat[0][2]-cos(roll_output)*h.mat[1][2], -sin(roll_output)*h.mat[0][1]+cos(roll_output)*h.mat[1][1]);
-    
-    pose_xYyPzR->x = x_output;
-    pose_xYyPzR->y = y_output;
-    pose_xYyPzR->z = z_output;
-    pose_xYyPzR->roll = roll_output;
-    pose_xYyPzR->pitch = pitch_output;
-    pose_xYyPzR->yaw = yaw_output;    
-    
-    return 1;
-}
 
-// xYyPzR 2 wYvPuR 2
-int xYyPzR2wYvPuR(droneMsgs::dronePose pose_xYyPzR, droneMsgs::dronePose* pose_wYvPuR) // GMR 2 LMrT
-{
-	 float x_input = pose_xYyPzR.x;
-    float y_input = pose_xYyPzR.y;
-    float z_input = pose_xYyPzR.z;
-    float roll_input = pose_xYyPzR.roll;
-    float pitch_input = pose_xYyPzR.pitch;
-    float yaw_input = pose_xYyPzR.yaw;
-    
-    float x_output = x_input;
-    float y_output = y_input;
-    float z_output = z_input;
-    float roll_output = yaw_input;
-    float pitch_output = pitch_input;
-    float yaw_output = roll_input;
-    
-    pose_wYvPuR->x = x_output;
-    pose_wYvPuR->y = y_output;
-    pose_wYvPuR->z = z_output;
-    pose_wYvPuR->roll = roll_output;
-    pose_wYvPuR->pitch = pitch_output;
-    pose_wYvPuR->yaw = yaw_output;
-    
-
-    return 1;
-
-
-
-}
-// wYvPuR 2 xYyPzR
-int wYvPuR2xYyPzR(droneMsgs::dronePose pose_wYvPuR, droneMsgs::dronePose* pose_xYyPzR) // LMrT 2 GMR
-{
-
-	 float x_input = pose_wYvPuR.x;
-    float y_input = pose_wYvPuR.y;
-    float z_input = pose_wYvPuR.z;
-    float roll_input = pose_wYvPuR.roll;
-    float pitch_input = pose_wYvPuR.pitch;
-    float yaw_input = pose_wYvPuR.yaw;
-    
-    float x_output = x_input;
-    float y_output = y_input;
-    float z_output = z_input;
-    float roll_output = yaw_input;
-    float pitch_output = pitch_input;
-    float yaw_output = roll_input;
-    
-    pose_xYyPzR->x = x_output;
-    pose_xYyPzR->y = y_output;
-    pose_xYyPzR->z = z_output;
-    pose_xYyPzR->roll = roll_output;
-    pose_xYyPzR->pitch = pitch_output;
-    pose_xYyPzR->yaw = yaw_output;
-
-    
-    return 1;
-
-
-
-}
-
-int createHomogMatrix_wYvPuR(cv::Mat *pHomogMat, double x, double y, double z, double yaw, double pitch, double roll) {
+int createHomogMatrix_wYvPuR(cv::Mat * const pHomogMat, double x, double y, double z, double yaw, double pitch, double roll) {
     int error_ocurred = 0;
 
     if ( (pHomogMat == NULL) || (pHomogMat->empty()) || (pHomogMat->size() != cv::Size(4,4)) ) {
@@ -252,7 +154,15 @@ int createMatHomogFromVecs(cv::Mat* pHomogMat, const cv::Mat &TransVec, const cv
     return error_ocurred;
 }
 
-
+int invertHomogMatrix(const cv::Mat &pHomogMat_orig, cv::Mat &pHomogMat_target) {
+    cv::Mat R, t;
+    pHomogMat_orig.copyTo(pHomogMat_target);
+    R = pHomogMat_target(cv::Rect(0,0,3,3));
+    t = pHomogMat_target(cv::Rect(3,0,1,3));
+    R = R.t(); // R = R.transpose();
+    t = -R*t;
+    return 1;
+}
 
 }
 
