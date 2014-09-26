88718d7f26e6fd2e6896c7c05e3ad76340bd6619
joselusl <jl.sanchez@upm.es>
2014-09-26 17:13:07 +0200

    fixing launchers and setup.sh

--- launch_dir/parrot_IBVSController_launcher_Release.sh
+++ launch_dir/parrot_IBVSController_launcher_Release.sh
@@ -14,7 +14,7 @@ gnome-terminal	 		\
 						roscore; 
 						exec bash\""  \
 	--tab --title "droneLogger"	--command "bash -c \"
-						roslaunch ./IBVS2013_launchfiles/parrotLogger.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK}; 
+						roslaunch parrotLogger parrotLogger.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK}; 
 						exec bash\""  \
 	--tab --title "ardrone_driver"	--command "bash -c \"
 						roslaunch ./ardrone_launchfiles/ardrone_outdoors.launch --wait drone_id_namespace:=drone$NUMID_DRONE;
@@ -24,11 +24,11 @@ gnome-terminal	 		\
 						roslaunch ./IBVS2013_launchfiles/opentld_for_IBVSController.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK}; 
 						exec bash\""  \
 	--tab --title "IBVSController"	--command "bash -c \"
-						roslaunch ./IBVS2013_launchfiles/parrot_IBVSController.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK}; 
+						roslaunch parrotController parrot_IBVSController.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK}; 
 						exec bash\""  \
 	--tab --title "EKF"	--command "bash -c \"
-	roslaunch ./IBVS2013_launchfiles/parrotStateEstimator.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK};
+	roslaunch parrotStateEstimator parrotStateEstimator.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK};
 						exec bash\""	\
 	--tab --title "IBVSCntInterf"	--command "bash -c \"
-						roslaunch ./IBVS2013_launchfiles/parrotIBVSBrainInterface.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK}; 
+						roslaunch parrotBrainInterface parrotIBVSBrainInterface.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK}; 
 						exec bash\""	&
--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #http://stackoverflow.com/questions/16011245/source-files-in-a-bash-script
-. ${IBVS_WORKSPACE}/setup.bash
+. ${IBVS_WORKSPACE}/devel/setup.bash
