d858fcad7e0542569a6a9fdb977a974eae6fef49
Jose Luis Sanchez Lopez <jl.sanchez@upm.es>
2014-09-28 15:19:12 +0200

    bugs fixed

--- launch_dir/parrot_IBVSController_launcher_Release.sh
+++ launch_dir/parrot_IBVSController_launcher_Release.sh
@@ -21,7 +21,7 @@ gnome-terminal	 		\
 						exec bash\""  \
 	--tab --title "OpenTLD"		--command "bash -c \"
 						env sleep 20s ; 
-						roslaunch ${IBVS_STACK}/launch_dir/opentld_for_IBVSController.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK}; 
+						roslaunch ${IBVS_STACK}/launch_dir/IBVS2013_launchfiles/opentld_for_IBVSController.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK}; 
 						exec bash\""  \
 	--tab --title "IBVSController"	--command "bash -c \"
 						roslaunch parrotController parrot_IBVSController.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK}; 
