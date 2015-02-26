814a42a95a44cb129c01f06d588b6d1fbb04073d
joselusl <jl.sanchez@upm.es>
2015-02-26 16:56:11 +0100

    added new sh wo logging and fixed a bug in the ardrone_launcher

---
+++ launch_dir/parrot_IBVSController_launcher_Release_NoLogging.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Load ROS_WORKSPACE, etc
+. ${IBVS_STACK}/setup.sh
+
+# Load some local variables
+NUMID_DRONE=0
+NETWORK_ROSCORE=localhost
+export ROS_MASTER_URI=http://localhost:11311
+OPEN_ROSCORE=1
+
+gnome-terminal	 		\
+	--tab --title "roscore" 	--command "bash -c \"
+						roscore; 
+						exec bash\""  \
+	--tab --title "ardrone_driver"	--command "bash -c \"
+						roslaunch ${IBVS_STACK}/launch_dir/ardrone_launchfiles/ardrone_outdoors.launch --wait drone_id_namespace:=drone$NUMID_DRONE;
+						exec bash\""  \
+	--tab --title "OpenTLD"		--command "bash -c \"
+						env sleep 20s ; 
+						roslaunch ${IBVS_STACK}/launch_dir/IBVS2013_launchfiles/opentld_for_IBVSController.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK}; 
+						exec bash\""  \
+	--tab --title "IBVSController"	--command "bash -c \"
+						roslaunch parrotController parrot_IBVSController.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK}; 
+						exec bash\""  \
+	--tab --title "EKF"	--command "bash -c \"
+	roslaunch parrotStateEstimator parrotStateEstimator.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK};
+						exec bash\""	\
+	--tab --title "IBVSCntInterf"	--command "bash -c \"
+						roslaunch parrotBrainInterface parrotIBVSBrainInterface.launch --wait drone_id_namespace:=drone$NUMID_DRONE drone_id_int:=$NUMID_DRONE my_stack_directory:=${IBVS_STACK}; 
+						exec bash\""	&
